9f13c432b857ddbe661f32dcc96c678edb0fbb91
Clifford Ressel <csressel@gmail.com>
2014-08-10 22:32:50 -0400

    tweaked model validations and added seed data
    Fixed variable confusion in some of the model helpers and added
    validations to only allow highlander teams/players to select UGC
    and 6v6 teams/players to select ESEA/ETF2L. Also added reasonably
    comprehensive seed data.

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,22 +4,31 @@ class Team < ActiveRecord::Base
   validates :name,      presence: true
   validates :game_type, presence: true, inclusion: { in %w(6v6 highlander) }
   validates :league,    presence: true, inclusion: { in %w(UGC ESEA ETF2L) }
+  validates :league,    presence: true, inclusion: { in %w(ESEA ETF2L) },                       if: :is_6v6?)
   validates :level,     presence: true, inclusion: { in %w(iron steel silver gold platinum) },  if: :is_UGC?
   validates :level,     presence: true, inclusion: { in %w(open invite) },                      if: :is_ESEA?
   validates :level,     presence: true, inclusion: { in %w(div1 div2 div3 div4 div5 div6) },    if: :is_ETF2L?
   validates :user_id,   presence: true
   validate  :has_class?
 
+  def is_highlander?
+    game_type == "highlander"
+  end
+
+  def is_6v6?
+    game_type == "6v6"
+  end
+
   def is_UGC?
-    game_type == "UGC"
+    league == "UGC"
   end
 
   def is_ESEA?
-    game_type == "ESEA"
+    league == "ESEA"
   end
 
   def is_ETF2L?
-    game_type == "ETF2L"
+    league == "ETF2L"
   end
 
   def has_class?
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user1 = User.create!(nick: "allen", profile: "blah", avatar: "http://www.alluserpics.com/data/thumbnails/14/christmas_cat.gif")
+@user1.save!
+@user2 = User.create!(nick: "bob", profile: "bleh", avatar: "http://www.alluserpics.com/data/thumbnails/14/christmas_cat.gif")
+@user2.save!
+@user3 = User.create!(nick: "cliff", profile: "blih", avatar: "http://www.alluserpics.com/data/thumbnails/14/christmas_cat.gif")
+@user3.save!
+@user4 = User.create!(nick: "dave", profile: "bloh", avatar: "http://www.alluserpics.com/data/thumbnails/14/christmas_cat.gif")
+@user4.save!
+@user5 = User.create!(nick: "eric", profile: "bluh", avatar: "http://www.alluserpics.com/data/thumbnails/14/christmas_cat.gif")
+@user5.save!
+
+@player1 = Player.create!(game_type: "highlander", league: "UGC", level: "steel", notes: "i b super pro shpee", user_id: 3, spy: true)
+@player1.save!
+@player2 = Player.create!(game_type: "6v6", league: "ESEA", level: "invite", user_id: 1, soldier: true, scout: true, sniper: true)
+@player2.save!
+
+@review1 = Review.create!(body: "This guy is soooooooo terrible at spy :P", user_id: 2, player_id: 3)
+@review1.save!
+@review2 = Review.create!(body: "True pro, pick him up quick", user_id: 3, player_id: 1)
+@review2.save!
+
+@team1 = Team.create!(name: "bballerz", tag: ">bb<", game_type: "highlander", league: "UGC", level: "steel", user_id: 5, spy: true)
+@team1.save!
+@team2 = Team.create!(name: "goingHAM", tag: "H4M", game_type: "6v6", league: "ETF2L", level: "div5", user_id: 2, scout: true, demoman: true)
+@team2.save!
